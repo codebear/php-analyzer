f7522cd2a577a3fe3239f6be30fe8db1b04cf862
Bjørn Tore Hagen <bjorn@cerum.no>
2022-05-20 20:24:35 +0200

    simplifying test with Result return type

--- src/tests/generics.rs
+++ src/tests/generics.rs
@@ -1,8 +1,12 @@
-use crate::{tests::evaluate_php_buffers, types::union::{DiscreteType, UnionType}, symbols::FullyQualifiedName};
+use crate::{
+    symbols::FullyQualifiedName,
+    tests::evaluate_php_buffers,
+    types::union::{DiscreteType, UnionType},
+};
 use std::ffi::OsString;
 
 #[test]
-fn test_gen() {
+fn test_gen() -> Result<(), &'static str> {
     let buffers: &[(OsString, OsString)] = &[(
         "foo/myinterface.php".into(),
         r#"<?php 
@@ -45,37 +49,36 @@ fn test_gen() {
     )];
     let result = evaluate_php_buffers(buffers.to_vec(), false);
     // eprintln!("RESULT: {:?}", &result);
-     if let Some(symbols) = result.symbol_data {
-        let func_data = symbols.functions.read().unwrap();
+    let symbols = result.symbol_data.ok_or("Missing symbols")?;
+    
+    let func_data = symbols.functions.read().unwrap();
 
-        let func_name: FullyQualifiedName = r"\test_return_class".into();
-        if let Some(func) = func_data.get(&func_name) {
-            let data = func.read().unwrap();
-            let expected = UnionType::parse_simple("X<String>".into()).unwrap();
-            assert_eq!(data.inferred_return_type, Some(expected));
-        } else {
-            assert!(false, "data of function test_return_class not found");
-        }
+    let func_name: FullyQualifiedName = r"\test_return_class".into();
 
-        let func_name: FullyQualifiedName = r"\test_return_method_string".into();
-        if let Some(func) = func_data.get(&func_name) {
-            let data = func.read().unwrap();
-            assert_eq!(data.inferred_return_type, Some(DiscreteType::String.into()));
-        } else {
-            assert!(false, "data of function test_return_method_string not found");
-        }
+    let func = func_data.get(&func_name).ok_or("data of function test_return_class not found")?;
+    {
+        let data = func.read().unwrap();
+        let expected = UnionType::parse_simple("X<String>".into()).unwrap();
+        assert_eq!(data.inferred_return_type, Some(expected));
+    }
 
-        let func_name: FullyQualifiedName = r"\test_return_method_int".into();
-        if let Some(func) = func_data.get(&func_name) {
-            let data = func.read().unwrap();
-            assert_eq!(data.inferred_return_type, Some(DiscreteType::Int.into()));
-        } else {
-            assert!(false, "data of function test_return_method_int not found");
-        }
+    let func_name: FullyQualifiedName = r"\test_return_method_string".into();
+    let func = func_data.get(&func_name).ok_or("data of function test_return_method_string not found")?;
+    {
+        let data = func.read().unwrap();
+        assert_eq!(data.inferred_return_type, Some(DiscreteType::String.into()));
+    }
+
+    let func_name: FullyQualifiedName = r"\test_return_method_int".into();
+    let func = func_data.get(&func_name).ok_or("data of function test_return_method_int not found")?;
+    {
+        let data = func.read().unwrap();
+        assert_eq!(data.inferred_return_type, Some(DiscreteType::Int.into()));
     }
     // assert_eq!(result.return_type, Some(DiscreteType::String.into()));
     assert_eq!(result.issues.len(), 0);
     //assert!(false)}
+    Ok(())
 }
 
 #[test]
