08e79c94beda83f2163a2c4f7bc2156a9293722e
Bjørn Tore Hagen <bjorn@cerum.no>
2022-09-18 23:04:19 +0200

    passthru

--- src/nodeanalysis/silence_expression.rs
+++ src/nodeanalysis/silence_expression.rs
@@ -4,23 +4,26 @@ use crate::{
 };
 
 impl SilenceExpressionNode {
-    pub fn read_from(&self, _state: &mut AnalysisState, _emitter: &dyn IssueEmitter) {
-        crate::missing!("{}.read_from(..)", self.kind());
+    pub fn read_from(&self, state: &mut AnalysisState, emitter: &dyn IssueEmitter) {
+        self.expr.read_from(state, emitter)
+        //crate::missing!("{}.read_from(..)", self.kind());
     }
 
     pub fn get_php_value(
         &self,
-        _state: &mut AnalysisState,
-        _emitter: &dyn IssueEmitter,
+        state: &mut AnalysisState,
+        emitter: &dyn IssueEmitter,
     ) -> Option<crate::value::PHPValue> {
-        crate::missing_none!("{}.get_php_value(..)", self.kind())
+        self.expr.get_php_value(state, emitter)
+        //crate::missing_none!("{}.get_php_value(..)", self.kind())
     }
 
     pub fn get_utype(
         &self,
-        _state: &mut AnalysisState,
-        _emitter: &dyn IssueEmitter,
+        state: &mut AnalysisState,
+        emitter: &dyn IssueEmitter,
     ) -> Option<UnionType> {
-        crate::missing_none!("{}.get_utype(..)", self.kind())
+        self.expr.get_utype(state, emitter)
+        //crate::missing_none!("{}.get_utype(..)", self.kind())
     }
 }
